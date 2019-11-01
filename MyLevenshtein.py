8399662fc7b2f79c7e27985b3223ec3fe1767814
KWASSI <kwassi-beno-charles.dokodjo@capgemini.com>
2019-11-01 10:15:48 +0100

    rewrite the distance metric and delete functions

--- MyLevenshtein.py
+++ MyLevenshtein.py
@@ -1,51 +1,17 @@
-def substitution(text1, text2):
-    """The two texts have same lengths"""
-
-    reference_text = list(text1)
-    new_text = list(text2)
-
-    assert reference_text == new_text
-    levenshtein_distance = 0
-
-    for i, letter in enumerate(reference_text):
-        if new_text[i] != letter:
-            levenshtein_distance += 1
-
-    return levenshtein_distance
+import numpy as np
 
 
-def deletion(text1, text2):
-    """Length of text1 is greater than length of text2"""
+def levenshtein_operation(M, Cost):
+    assert M.shape == (Cost.shape[0] + 1, Cost.shape[1] + 1)
+    operations = M
 
-    reference_text = list(text1)
-    new_text = list(text2)
+    for i in range(1, M.shape[0] + 1):
+        operations[i] = [
+            min(M[i - 1, j] + 1, M[i, j - 1] + 1, M[i - 1, j - 1] + Cost(i - 1, j - 1))
+            for j in range(1, M.shape[1] + 1)
+        ]
 
-    assert reference_text > new_text
-    levenshtein_distance = len(reference_text) - len(new_text)
-    reference_text = reference_text[: len(new_text)]
-
-    for i, letter in enumerate(reference_text):
-        if new_text[i] != letter:
-            levenshtein_distance += 1
-
-    return levenshtein_distance
-
-
-def insertion(text1, text2):
-    """Length of text2 is greater than length of text1"""
-
-    reference_text = list(text1)
-    new_text = list(text2)
-
-    assert reference_text < new_text
-    levenshtein_distance = len(new_text) - len(reference_text)
-    new_text = new_text[: len(reference_text)]
-
-    for i, letter in enumerate(reference_text):
-        if new_text[i] != letter:
-            levenshtein_distance += 1
-
-    return levenshtein_distance
+    return operations
 
 
 def Levenshtein(text1, text2):
@@ -53,14 +19,37 @@ def Levenshtein(text1, text2):
 
     levenshtein_distance = 0
 
-    if len(text1) == len(text2):
-        levenshtein_distance = substitution(text1, text2)
+    reference_text = list(text1)
+    new_text = list(text2)
 
-    if len(text1) > len(text2):
-        levenshtein_distance = insertion(text1, text2)
+    if len(reference_text) == 0:
+        levenshtein_distance = len(new_text)
 
-    if len(text1) < len(text2):
-        levenshtein_distance = deletion(text1, text2)
+    if len(new_text) == 0:
+        levenshtein_distance = len(reference_text)
 
-    assert levenshtein_distance >= 0
+    if len(reference_text) > 0 and len(new_text) > 0:
+
+        if reference_text == new_text:
+            levenshtein_distance = 0
+
+        else:
+
+            operations = np.zeros((len(reference_text) + 1, len(new_text) + 1))
+            operations[0] = range(len(new_text))
+            operations[:, 0] = range(len(reference_text))
+
+            Cost = np.zeros((len(reference_text), len(new_text)))
+
+            for i, letter in enumerate(reference_text):
+                Cost[i] = [
+                    1 if letter != new_text[j] else 0 for j in range(1, len(new_text))
+                ]
+
+            operations = levenshtein_operation(operations, Cost)
+
+            levenshtein_distance = operations[operations.shape[0], operations.shape[1]]
+
+            assert levenshtein_distance >= 0
+
     return levenshtein_distance
